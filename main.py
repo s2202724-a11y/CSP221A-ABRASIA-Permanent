9e58ee2d186c98dedf23030a227d2d775025f8e0
s2202724-a11y <s2202724@usls.edu.ph>
2026-08-13 22:51:48 +0800

    Added the fleet report function and test mixed robot fleet

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from robot import Robot
-# Testinggg
-class CleaningRobot(Robot):
-    def perform_task(self):
-        return f"{self.name} is cleaning the house."
+from cleaningRobot import CleaningRobot
+from droneRobot import DroneRobot
 
-robot = CleaningRobot("Testing Robot", 120)
 
-print(robot)
-print(robot.perform_task())
+# Function of fleet report
+def fleet_report(robots: list):
+    print("Fleet Report:")
+    for robot in robots:
+        print(str(robot))
+
+# Create a test instance
+cleanRobo = CleaningRobot("Testing Robot", 120, dust_capacity=40)
+droneRobo = DroneRobot("Flying Robot", 80, max_alt=200)
+
+print(cleanRobo)
+print(cleanRobo.perform_task())
+print(cleanRobo)  # Shows battery reduced after performing task
+
+print(droneRobo)
+print(droneRobo.perform_task())
+print(droneRobo)  # Shows battery reduced after performing task
+
+fleet = [
+    cleanRobo,
+    droneRobo,
+    CleaningRobot("Mop", 45, dust_capacity=20),
+]
+
+fleet_report(fleet)
